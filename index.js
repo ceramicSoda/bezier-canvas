e0f472e5511a2154f461aa99d0fc798ab9e8c6c1
ceramicSoda <notyourbusiness@notexist.com>
2024-01-21 21:34:30 +0200

    Bezier curves formula

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const lerp = (x, y, a) => x * (1 - a) + y * a;
-const clamp = (a, min = 0, max = 1) => Math.min(max, Math.max(min, a));
-const invlerp = (x, y, a) => clamp((a - x) / (y - x));
-const range = (x1, y1, x2, y2, a) => lerp(x2, y2, invlerp(x1, y1, a));
 
+
+function cubicBezierPoint3D(t, p0, p1, p2, p3) {
+    let c1 = Math.pow(1 - t, 3);
+    let c2 = 3 * Math.pow(1 - t, 2) * t;
+    let c3 = 3 * (1 - t) * Math.pow(t, 2);
+    let c4 = Math.pow(t, 3);
+
+    let x = c1 * p0[0] + c2 * p1[0] + c3 * p2[0] + c4 * p3[0];
+    let y = c1 * p0[1] + c2 * p1[1] + c3 * p2[1] + c4 * p3[1];
+    let z = c1 * p0[2] + c2 * p1[2] + c3 * p2[2] + c4 * p3[2];
+
+    return [x, y, z];
+}
+
+const getTrackPoint = (pointsArr, progress) => {
+    pointsArr.forEach((curve, cur, curves) => {
+        let prev = cur - 1,
+            next = cur + 1;
+        if (cur == 0)
+            prev = curves.length - 1;
+        if (cur == (curves.length - 1))
+            next = 0;
+        
+    })
+}
